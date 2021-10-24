dfc5c6f4eb85b0704bbba4473492837d38a357cd
yiorgospx <gpapacharalambous@dits.dmrid.gov.cy>
2021-10-25 01:01:49 +0300

    bah bah charlie

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello from hellas-space +++>>');
+  res.send('Yes sir, Yes sir, three bags full!');
 })
 
 app.listen(8080, function () {
-  console.log('Starting hellas-www ...');
+  console.log('Starting bah-bah-charlie-have-you-any-wool ...');
 })
