1cd19cadd716f398718c27dc7f34eec97712c20e
yiorgospx <gpapacharalambous@dits.dmrid.gov.cy>
2021-10-25 00:33:50 +0300

    spacex

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello from space!');
+  res.send('Hello from spaceX!');
 })
 
 app.listen(8080, function () {
-  console.log('Starting hellas-ws ...');
+  console.log('Starting hellas-space-x ...');
 })
