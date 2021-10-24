6ec3177902fd1cfb0d7bbc3553c0e023ac28f5d1
yiorgospx <gpapacharalambous@dits.dmrid.gov.cy>
2021-10-25 00:13:19 +0300

    git

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello world from GP!');
+  res.send('Hello world from!!!!!');
 })
 
 app.listen(8080, function () {
-  console.log('Starting hellas-www server ...');
+  console.log('Starting hellas-web-server ...');
 })
