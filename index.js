247fe52ba2141955c1f778882e1822df9e60b664
yiorgospx <gpapacharalambous@dits.dmrid.gov.cy>
2021-10-25 00:14:46 +0300

    pushes

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello world from!!!!!');
+  res.send('Hello world @@@ from!!!!!');
 })
 
 app.listen(8080, function () {
-  console.log('Starting hellas-web-server ...');
+  console.log('Starting hellas-ws ...');
 })
