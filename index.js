0df273ab2e9bc4a93725ea5e6235ef65b0640016
yiorgospx <gpapacharalambous@dits.dmrid.gov.cy>
2021-10-25 00:15:40 +0300

    space

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello world @@@ from!!!!!');
+  res.send('Hello world @=>=> from!!!!!');
 })
 
 app.listen(8080, function () {
