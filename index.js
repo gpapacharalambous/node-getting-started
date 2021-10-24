d3ddebc679c92f7243740209c72727ae91ae5c99
yiorgospx <gpapacharalambous@dits.dmrid.gov.cy>
2021-10-25 00:51:07 +0300

    efd

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello from hellas-space-academy');
+  res.send('Hello from hellas-space +++>>');
 })
 
 app.listen(8080, function () {
-  console.log('Starting hellas-space-academy ...');
+  console.log('Starting hellas-www ...');
 })
