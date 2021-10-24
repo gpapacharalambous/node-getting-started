db4905164870f5eeb87e58d087daf1952b9fad4d
yiorgospx <gpapacharalambous@dits.dmrid.gov.cy>
2021-10-24 23:31:14 +0300

    depl

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,6 @@ RUN npm install
 
 COPY index.js .
 
-EXPOSE 3000
+EXPOSE 8080
 
 CMD npm start
--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.get('/', function (req, res) {
   res.send('Hello world!');
 })
 
-app.listen(3000, function () {
+app.listen(8080, function () {
   console.log('Starting hello-world server...');
 })
