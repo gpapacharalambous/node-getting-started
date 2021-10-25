3ce2e210bf85fe90628be5afd33b78900e52b8e1
yiorgospx <gpapacharalambous@dits.dmrid.gov.cy>
2021-10-25 13:12:21 +0300

    gp change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('New deployment is up for kyp!');
+  res.send('New deployment is up for george!');
 })
 
 app.listen(8080, function () {
