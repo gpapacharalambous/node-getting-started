00757c3bd299d027f5a209b0b9beed0370ff4a7a
yiorgospx <gpapacharalambous@dits.dmrid.gov.cy>
2021-10-25 10:51:10 +0300

    push

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('New deployment is up!');
+  res.send('New deployment is up for kyp!');
 })
 
 app.listen(8080, function () {
